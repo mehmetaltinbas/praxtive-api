449cc5e1b207c38142197aa95d25171c9160a634
Mehmet Altınbaş <altnbsmehmet@icloud.com>
2026-04-22 01:43:40 +0300

    enhancement(billing): default currency constant as USD

--- src/db/models/plan.model.ts
+++ src/db/models/plan.model.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose';
+import { DEFAULT_CURRENCY } from 'src/billing/constants/default-currency.constant';
 import { SubscriptionModel } from 'src/db/models/subscription.model';
 import { PlanName } from 'src/plan/enums/plan-name.enum';
 import { PlanDocument } from 'src/plan/types/plan-document.interface';
@@ -7,7 +8,7 @@ const schema = new mongoose.Schema(
     {
         name: { type: String, enum: Object.values(PlanName), unique: true, required: true },
         monthlyPrice: { type: Number, required: true },
-        currency: { type: String, required: true },
+        currency: { type: String, required: true, default: DEFAULT_CURRENCY },
         monthlyCredits: { type: Number, required: true },
         maximumCredits: { type: Number, required: true },
         maxSources: { type: Number, required: true },
