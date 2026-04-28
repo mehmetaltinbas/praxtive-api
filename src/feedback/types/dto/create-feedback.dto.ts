344b6ad897c63784cbe6cbbdffc99f0dbf43adca
Mehmet Altınbaş <altnbsmehmet@icloud.com>
2026-04-28 06:46:55 +0300

    churn(feedback): min/max feedback content lengths

--- src/feedback/types/dto/create-feedback.dto.ts
+++ src/feedback/types/dto/create-feedback.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { MAX_FEEDBACK_LENGTH } from 'src/feedback/constants/max-feedback-length.constant';
+import { MIN_FEEDBACK_LENGTH } from 'src/feedback/constants/min-feedback-length.constant';
 
 export class CreateFeedbackDto {
-    @Length(10, 500)
+    @Length(MIN_FEEDBACK_LENGTH, MAX_FEEDBACK_LENGTH)
     @IsString()
     @IsNotEmpty()
     readonly content!: string;
